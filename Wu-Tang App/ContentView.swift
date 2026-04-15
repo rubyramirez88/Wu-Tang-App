057855f42b5a1c558aa949e4acc44305bcae05df
rubyramirez88 <ruizruby160@gmail.com>
2026-04-15 12:41:56 -0700

    Initial Commit

---
+++ Wu-Tang App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Wu-Tang App
+//
+//  Created by RAMIREZ, RUBY on 4/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Wu-Tang App/Wu_Tang_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wu_Tang_AppApp.swift
+//  Wu-Tang App
+//
+//  Created by RAMIREZ, RUBY on 4/15/26.
+//
+
+import SwiftUI
+
+@main
+struct Wu_Tang_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
